9228ea85923f6a481609103572bedf442b14a693
Lucass307 <lucas.defaud@gmail.com>
2023-12-01 14:44:43 +0100

    code avec test

--- src/ls.py
+++ src/ls.py
@@ -1,4 +1,5 @@
 import os
+import unittest
 
 def ls(directory="."):
     files = os.listdir(directory)
@@ -6,5 +7,17 @@ def ls(directory="."):
     for file in files:
         print(file)
 
+class TestLSFunction(unittest.TestCase):
+    def test_ls_default_directory(self):
+        # Test la fonction ls avec le répertoire par défaut
+        with self.assertOutput(match=".*", msg="No output from ls function."):
+            ls()
 
-ls()
+    def test_ls_custom_directory(self):
+        # Test la fonction ls avec un répertoire spécifique
+        custom_directory = "/chemin/vers/un/autre/repertoire"
+        with self.assertOutput(match=".*", msg="No output from ls function."):
+            ls(custom_directory)
+
+if __name__ == "__main__":
+    unittest.main()

c11f0da08284e8737796da8cc31f407b7e118059
Lucass307 <lucas.defaud@gmail.com>
2023-12-01 15:00:13 +0100

    testV3

--- src/test_ls.py
+++ src/test_ls.py
@@ -1,18 +1,44 @@
-# test_ls_function.py
+import os
 import unittest
-from ls import ls
+from io import StringIO
+import sys
+
+def ls(directory="."):
+    files = os.listdir(directory)
+
+    for file in files:
+        print(file)
 
 class TestLSFunction(unittest.TestCase):
+    def setUp(self):
+        # Rediriger la sortie standard vers un StringIO
+        self.held_output = StringIO()
+        sys.stdout = self.held_output
+
+    def tearDown(self):
+        # Restaurer la sortie standard
+        sys.stdout = sys.__stdout__
+
     def test_ls_default_directory(self):
-        # Test la fonction ls avec le répertoire par défaut
-        with self.assertOutput(match=".*", msg="No output from ls function."):
-            ls()
+        # Appeler la fonction ls
+        ls()
+
+        # Récupérer la sortie
+        output = self.held_output.getvalue().strip()
+
+        # Vérifier si la sortie n'est pas vide
+        self.assertTrue(output, "No output from ls function.")
 
     def test_ls_custom_directory(self):
-        # Test la fonction ls avec un répertoire spécifique
-        custom_directory = "/chemin/vers/un/autre/repertoire"
-        with self.assertOutput(match=".*", msg="No output from ls function."):
-            ls(custom_directory)
+        # Appeler la fonction ls avec un répertoire spécifique
+        custom_directory = "C:/Users/"
+        ls(custom_directory)
+
+        # Récupérer la sortie
+        output = self.held_output.getvalue().strip()
+
+        # Vérifier si la sortie n'est pas vide
+        self.assertTrue(output, "No output from ls function.")
 
 if __name__ == "__main__":
     unittest.main()

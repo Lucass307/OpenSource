854e8a8d91956838f81bb558bc76c904a9e118f1
Yoni FIRROLONI <pro@yoni-firroloni.com>
2023-12-01 17:51:01 +0100

    err: add details when not exist

--- src/ls.py
+++ src/ls.py
@@ -1,11 +1,11 @@
 # ls_function.py
 import os
 
+
 def ls(directory="."):
     if os.path.exists(directory):
         files = os.listdir(directory)
         for file in files:
             print(file)
     else:
-        print(f"Directory '{directory}' does not exist.")
-
+        print(f"Directory '{directory}' does not exist. Please try again.")
